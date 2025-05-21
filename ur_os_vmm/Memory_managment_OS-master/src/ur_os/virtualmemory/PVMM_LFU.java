c9df9a2f4a48d2c9e7098d7c8444f703671c4f67
jalvarado12 <105568949+jalvarado12@users.noreply.github.com>
2025-05-21 14:59:39 -0500

    Update PVMM_LFU.java

--- ur_os_vmm/Memory_managment_OS-master/src/ur_os/virtualmemory/PVMM_LFU.java
+++ ur_os_vmm/Memory_managment_OS-master/src/ur_os/virtualmemory/PVMM_LFU.java
@@ -27,18 +27,21 @@ public class PVMM_LFU extends ProcessVirtualMemoryManager{
     temp.removeLast(); // In order to not count the page we have to add
 
     for (Integer m : temp) {
-        // pages count part
         if (!pages.contains(m)) {
             pages.add(m);
         }
         if (pages.size() > loaded) {
             LinkedList<Integer> temp2 = new LinkedList<Integer>(pages);
-            temp2.removeLast(); // In order to not count the element to add
+            temp2.removeLast(); //not count the element to add
             victim = minVictim(frequency, temp2);
             pages.remove(victim);
         }
 
-        frequency.put(m, frequency.getOrDefault(m, 0) + 1);
+        if (frequency.containsKey(m)) {
+            frequency.put(m, frequency.get(m) + 1);
+        } else {
+            frequency.put(m, 1);
+}
     }
 
     return minVictim(frequency, pages);
@@ -48,14 +51,14 @@ public int minVictim(Map<Integer, Integer> frequency, LinkedList<Integer> pages)
     Integer min = Integer.MAX_VALUE;
     Integer victim = -1;
 
-    // searching min
+    //  min
     for (Integer p : pages) {
         if (frequency.get(p) < min) {
             min = frequency.get(p);
         }
     }
 
-    // in order to select the first in (in case of draw)
+    // draw
     for (Integer p : pages) {
         if (frequency.get(p) == min) {
             victim = p;
